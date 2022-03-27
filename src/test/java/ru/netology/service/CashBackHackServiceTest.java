934e7fbe184bd4666927e7fc3c91c7cf93edafb0
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-03-27 13:41:11 +1000

    AutoTests

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -5,22 +5,61 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 
-class CashBackHackServiceTest {
+public class CashBackHackServiceTest {
 
     @Test
-
-    public static void shouldC(){
+    public static void shouldCalculate() {
         CashBackHackService service = new CashBackHackService();
 
-        int amount = 100;
+        int amount = 1500;
 
-        int expected = 1000;
+        int expected = 500;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
-
+        assertEquals(actual, expected);
 
 
     }
+
+    @Test
+    public static void shouldCalculate1000() {
+        CashBackHackService service = new CashBackHackService();
+
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    public static void shouldCalculate1001() {
+        CashBackHackService service = new CashBackHackService();
+
+        int amount = 1001;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    public static void shouldCalculate999() {
+        CashBackHackService service = new CashBackHackService();
+
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+
+    }
+
+
+
 
 }
