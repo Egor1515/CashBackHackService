600c27b6d68c2ad8196f25a2b61815b8e37cbb6d
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-11 19:12:00 +1000

    Push

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -1,25 +1,7 @@
 package ru.netology.service;
 
-
-import org.junit.Assert;
-import org.junit.Test;
-
-
-public class CashBackHackServiceTest {
-    CashBackHackService service = new CashBackHackService();
-
-
-    @Test
-
-    public void shouldCalculate() {
-        int amount = 1500;
-
-
-        int expected = 500;
-        int actual = service.remain(amount);
-
-
 import org.testng.annotations.Test;
+
 
 import static org.testng.Assert.assertEquals;
 
@@ -37,49 +19,9 @@ public class CashBackHackServiceTest {
         assertEquals(actual, expected);
 
 
-
     }
 
     @Test
-
-    public void shouldCalculate1() {
-        int amount = 1000;
-
-        int expected = 0;
-        int actual = service.remain(amount);
-
-
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculate2() {
-        int amount = 999;
-
-
-        int expected = 1;
-        int actual = service.remain(amount);
-
-
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculate3() {
-        int amount = 1001;
-
-
-        int expected = 999;
-        int actual = service.remain(amount);
-
-
-        Assert.assertEquals(expected, actual);
-    }
-
-
-}
-
-
     public static void shouldCalculate1000() {
         CashBackHackService service = new CashBackHackService();
 
@@ -122,4 +64,3 @@ public class CashBackHackServiceTest {
 
 
 }
-
